13c9dc78abcee5c6f0594ebf38ac9db8974b0210
Croubs <diegoantoniolopezruelas@gmail.com>
2023-06-17 11:23:41 -0600

    First commit

---
+++ test-app-IdeaHub/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'Inicio',
+    component: InicioComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
